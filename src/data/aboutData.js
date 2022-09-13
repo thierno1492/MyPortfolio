fe0ce63061db84c9b0b5f4f48008ba1afd72d1a0
Thierno <thierno1492@gmail.com>
2022-09-13 11:48:47 +0200

    nombre

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "Mi nombre es Thierno Boubacar Sall. Soy un desarrollador web apasionado por las nuevas tecnologías.",
+    description1: "Mi nombre es Thierno Diallo. Soy un desarrollador web apasionado por las nuevas tecnologías.",
     description2: "Me gustan los retos y el trabajo en equipo. Me adapto fácilmente a nuevos entornos. Tengo más de un año en  desarrollo web. Me encanta aprender y explorar nuevos retos.",
     image: 2
 }
