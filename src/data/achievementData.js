9ea5a14e8ee7ac75ec9640cf72a2c1fe2476c996
Thierno <thierno1492@gmail.com>
2022-09-15 10:01:18 +0200

    h

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -4,7 +4,7 @@ export const achievementData = {
         {
             id : 1,
             title : 'App Platform Builder',
-            details : 'Los Constructores de aplicaciones de plataforma certificados tienen las aptitudes y los conocimientos para diseñar, construir e implementar aplicaciones personalizadas empleando las funciones de personalización declarativas de Salesforce Platform.',
+            details : 'tienen las aptitudes y los conocimientos para diseñar, construir e implementar aplicaciones personalizadas empleando las funciones de personalización declarativas de Salesforce .',
             date : 'junio de 2022 - Presente',
             field : 'Auto',
             image : 'https://media-exp1.licdn.com/dms/image/C5612AQFjex3jQXRv1g/article-cover_image-shrink_720_1280/0/1642712686990?e=1664409600&v=beta&t=n-6hhd-rcDvhNuVa475OYUAWnJCwAmunVxIoLajPB78'
@@ -13,7 +13,7 @@ export const achievementData = {
         {
             id : 2,
             title : 'Salesforce Certified Platform Developer I',
-            details : 'tienen los conocimientos para desarrollar implementar lógica de negocio personalizada e interfaces personalizadas empleando las funciones programáticas de Lightning Platform. Saben cuándo utilizar métodos declarativos frente a los programáticos.',
+            details : 'tienen las aptitudes desarrollar implementar lógica de negocio personalizada e interfaces personalizadas empleando las funciones programáticas de Lightning Platform.',
             date : 'mayo de 2022 - Presente',
             field : 'dev',
             image : 'https://blog.facialix.com/wp-content/uploads/2022/07/4792472_3c72.jpg'
