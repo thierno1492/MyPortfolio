37cb806b3cff49aa22fdfaccbd3b199517e45478
Thierno Diallo <78595975+thierno1492@users.noreply.github.com>
2024-04-16 15:50:16 +0200

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Quién soy",
     description1: "Mi nombre es Thierno Diallo . Soy un desarrollador web apasionado por las nuevas tecnologías.",
-    description2: "Me gustan los retos y el trabajo en equipo. Me adapto fácilmente a nuevos entornos. Tengo más de un año en  desarrollo web. Me encanta aprender y explorar nuevos retos.",
+    description2: "Me gustan los retos y el trabajo en equipo. Me adapto fácilmente a nuevos entornos. Tengo más de 3 año en  desarrollo web. Me encanta aprender y explorar nuevos retos.",
     image: 2
 }
